0961f031851126fd4749d32e7eff61c0defba9d2
chenby <627648471@qq.com>
2016-04-25 18:44:38 +0800

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path')
+var webpack = require('webpack');
+var env="test";
+module.exports = {
+  entry: [
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV':JSON.stringify(env)
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [ 'babel' ],
+        exclude: /node_modules/,
+        include: __dirname
+      },{ 
+        test: /\.css$/, 
+        exclude: /\.useable\.css$/,
+         loader: "style!css" 
+       }
+    ]
+  }
+}
+
+
+// When inside Redux repo, prefer src to compiled version.
+// You can safely delete these lines in your project.
+// var reduxSrc = path.join(__dirname, '..', '..', 'src')
+// var reduxNodeModules = path.join(__dirname, '..', '..', 'node_modules')
+// var fs = require('fs')
+// if (fs.existsSync(reduxSrc) && fs.existsSync(reduxNodeModules)) {
+//   // Resolve Redux to source
+//   module.exports.resolve = { alias: { 'redux': reduxSrc } }
+//   // Compile Redux from source
+//   module.exports.module.loaders.push({
+//     test: /\.js$/,
+//     loaders: [ 'babel' ],
+//     include: reduxSrc
+//   })
+// }
